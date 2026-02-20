263fbe0561d696186fe4957935f7a6c5499f5791
yassir <yassirmelendezaaviles14@gmail.com>
2026-02-20 18:22:58 -0300

    comillas error

--- index.php
+++ index.php
@@ -17,23 +17,23 @@
 
 <?php if (isset($_SESSION['success'])): ?> 
 
-```
+
 <div class="bg-green-100 text-green-700 p-3 rounded mb-4">
     <?= is_string($_SESSION['success']) ? $_SESSION['success'] : "Operación realizada correctamente." ?>
 </div> 
 <?php unset($_SESSION['success']); ?>
-```
+
 
 <?php endif; ?>
 
 <?php if (isset($_SESSION['error'])): ?>
 
-```
+
 <div class="bg-red-100 text-red-700 p-3 rounded mb-4">
     <?= $_SESSION['error']; ?>
 </div>
 <?php unset($_SESSION['error']); ?>
-```
+
 
 <?php endif; ?>
 
